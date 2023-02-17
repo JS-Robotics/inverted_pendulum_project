64fbbe4b660766071570070549275d1920891a7a
Sondre <sjohnsen.valle@gmail.com>
2023-02-17 21:20:39 +0100

    Trying to figure out sphinx

---
+++ docs/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Inverted Pendulum'
+copyright = '2023, Sondre Johnsen'
+author = 'Sondre Johnsen'
+release = 'v0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+# Math support> https://sphinx-rtd-trial.readthedocs.io/en/latest/ext/math.html#module-sphinx.ext.mathbase
+# Read the docs theme: https://sphinx-rtd-theme.readthedocs.io/en/stable/index.html
+# Requires to install rtd theme: pip install sphinx_rtd_theme
+extensions = ['sphinx_rtd_theme',
+              'sphinx.ext.mathjax']
+
+# 'sphinx.ext.imgmath'
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,49 @@
+.. Inverted Pendulum documentation master file, created by
+   sphinx-quickstart on Fri Feb 17 20:38:28 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Inverted Pendulum Documentation
+=============================================
+
+Let's try this out
+
+Inline math is used with Since Pythagoras, we know that :math:`a^2 + b^2 = c^2`.
+
+Math blocks are used with
+
+.. math::
+
+   (a + b)^2 = a^2 + 2ab + b^2
+
+   (a - b)^2 = a^2 - 2ab + b^2
+
+New math block test
+
+.. math::
+
+   a = \inf \int^{a}_{b} x dx
+
+.. math::
+
+   \frac{ \sum_{t=0}^{N}f(t,k) }{N}
+
+
+Now lets try a coding block
+
+   int a = 0;
+   int b = 1;
+   float c = static_cast<float>(b);
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
