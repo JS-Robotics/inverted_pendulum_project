8b8e1713914e5605c9dc6738cb2f9fc1d1b4f484
sondre <sondre.johnsen@hiveautonomy.no>
2023-03-04 17:41:35 +0100

    Working on cart control node. Have given up on getting autocomplete working with pycharm and ros2

--- ros2/src/ivp_control/ivp_control/control.py
+++ ros2/src/ivp_control/ivp_control/control.py
@@ -1,1 +1,40 @@
-#include 
+import rclpy
+from rclpy.node import Node
+from rclpy import qos
+from geometry_msgs.msg import Vector3
+
+from cart_control import CartControl
+
+
+class Control(Node):
+
+    def __init__(self):
+        super().__init__('cart_control_node')
+        self.cart_control: CartControl = CartControl()
+        self.subscriber_ = None
+        self.pendulum_pos: float = 0.0
+        self.pendulum_vel: float = 0.0
+
+    def __del__(self):
+        # self.cart_control.set_state_idle()
+        print("Drive was set to idle")
+
+    def run_once(self):
+        self.get_logger().info(
+            f'Pendulum: pos[rad], vel[rad/s]: "{round(self.pendulum_pos, 4)}", "{round(self.pendulum_vel, 4)}"')
+
+    def configure(self):
+        self.subscriber_ = self.create_subscription(
+            Vector3,
+            '/ivp/pendulum_state',
+            self.listener_callback,
+            qos.qos_profile_sensor_data)
+
+    def prepare_motor(self):
+        self.cart_control.connect()
+        self.cart_control.calibrate()
+        self.cart_control.homing()
+
+    def listener_callback(self, pendulum_state):
+        self.pendulum_pos = pendulum_state.x
+        self.pendulum_vel = pendulum_state.y
--- ros2/src/ivp_control/ivp_control/main.py
+++ ros2/src/ivp_control/ivp_control/main.py
@@ -1,5 +1,33 @@
-def main():
-    print('Hi from ivpasdasd_control.')
+from ivp_control.control import Control
+import rclpy
+import time
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    control_node = Control()
+    control_node.prepare_motor()
+
+    executor = rclpy.executors.SingleThreadedExecutor()
+    executor.add_node(control_node)
+
+    control_node.configure()
+
+    try:
+        while rclpy.ok():
+            control_node.run_once()
+            executor.spin_once()
+            time.sleep(0.001)
+
+    except KeyboardInterrupt:
+        print('interrupted')
+
+    finally:
+        print("Finally executed")
+
+    control_node.destroy_node()
+    # rclpy.shutdown() # Causes problems
 
 
 if __name__ == '__main__':
