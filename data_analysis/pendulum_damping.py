267a7d433904f2fa7ae596f095f30a922838a935
Sondre <sjohnsen.valle@gmail.com>
2023-03-27 18:01:25 +0200

    Working on cleaning up damping files

--- data_analysis/pendulum_damping.py
+++ data_analysis/pendulum_damping.py
@@ -61,22 +61,6 @@ pos_old = pos - 100
 points = []
 points_time = []
 
-# for i in range(49):
-#     while pos >= pos_old:
-#         pos_old = pos
-#         step += 1
-#         pos = position[step]
-#
-#     points.append(position[step - 1])
-#     points_time.append((epoch_ns[step - 1] - epoch_ns[0]) / 10 ** 9)
-#
-#     while pos <= pos_old:
-#         pos_old = pos
-#         step += 1
-#         pos = position[step]
-
-    #points.append(position[step - 1])
-    #points_time.append((epoch_ns[step - 1] - epoch_ns[0]) / 10 ** 9)
 n = 15
 for i in range(n, len(position)-n):
     if position[i] == max(position[i-n:i+n]):
@@ -103,14 +87,6 @@ print(len(points))
 print(points)
 c = d/(np.sqrt(d**2 + (n_c*2*np.pi)**2))
 dt = 2
-t = np.zeros(45)
-
-timer = 0
-for step in range(len(t)):
-    t[step] = timer
-    timer += dt
-
-
 
 
 # Poly fit
@@ -118,33 +94,19 @@ points = np.array(points)
 points_time = np.array(points_time)
 A, B = np.polyfit(points_time, np.log(points), 1)
 
-c1 = 0.050
-c2 = 0.0001
-
-c3 = 0.075
-c4 = 0.0325
-c5 = 0.000001
-c6 = 0.04315
-e = (points[0])*np.exp(-c1*t)
-e_l = (points[0])*np.exp(A*t)
-e_2 = points[0]*np.exp(-c6*t)
-
-t = t + points_time[0]
 
 T = np.median(points_time[1:] - points_time[0:np.size(points)-1])
 print(T)
 print(points_time[1:] - points_time[0:np.size(points)-1])
 print(points_time[5] - points_time[4])
-t_new = np.zeros(1000)
+t_new = np.zeros(794)
 dt = 0.08
 timer = 0
 
 for step in range(len(t_new)):
     t_new[step] = timer
     timer += dt
-plotter = np.exp(A*t_new + B) * np.sin(2*np.pi/T*t_new + 0.95)
-e_new = (A*t_new + np.exp(B)) * np.sin(2*np.pi/T*t_new + 0.25)
-
+reconstructed = np.exp(A*t_new + B) * np.sin(2*np.pi/T*t_new + 0.95)
 
 # Numerical sim
 # display the plot
@@ -168,27 +130,18 @@ for i in t_new:
 
 
 sample_set = ax.plot((epoch_ns - epoch_ns[0]) / 10 ** 9, position, color='r', label='Sample set')
-ax.plot(points_time, points, marker='o', color='b')
-# ax.plot(t, e, color='c')
+line_reconstructed = ax.plot(t_new, reconstructed, color='y', label='Reconstructed')
+line_sim = ax.plot(t_new, pos_samples, color='c', label='Simulation model')
 
-ax.plot(t_new, pos_samples, color='c')
-line_reconstructed = ax.plot(t, e_l, color='g', label='Reconstructed')
-# ax.plot(t, e_2, color='k')
-line_sim = ax.plot(t_new, plotter, color='y', label='Simulation model')
-# ax.plot(t_new, e_new, color='k')
-# ax.plot((epoch_ns1 - epoch_ns1[0]) / 10 ** 9, position1, color='b')
-# ax.plot((epoch_ns2-epoch_ns2[0])/10**9, position2, color='g')
-# ax.plot((epoch_ns3-epoch_ns3[0])/10**9, position3, color='k')
-# ax.plot((epoch_ns4-epoch_ns4[0])/10**9, position4, color='c')
 
 ax.legend(handles=[sample_set[0], line_reconstructed[0], line_sim[0]])
 
 ax.set_title('plot')
 
 
-plt.figure()
-plt.plot(points_time, np.log(points), 'ks')
-plt.plot(points_time, A*points_time + B, 'r')
+# plt.figure()
+# plt.plot(points_time, np.log(points), 'ks')
+# plt.plot(points_time, A*points_time + B, 'r')
 
 plt.show()
 
