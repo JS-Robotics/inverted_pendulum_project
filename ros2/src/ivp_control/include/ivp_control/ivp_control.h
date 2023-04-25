666db02e7ee5a83bdbfb1fc324cbfa66856ea3c3
sondre <sjohnsen.valle@gmail.com>
2023-04-25 21:00:23 +0200

    Started on swing-up algorithm

--- ros2/src/ivp_control/include/ivp_control/ivp_control.h
+++ ros2/src/ivp_control/include/ivp_control/ivp_control.h
@@ -7,8 +7,17 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/float32.hpp"
+#include "geometry_msgs/msg/vector3.hpp"
 
 namespace ivp {
+
+struct State {
+  double angle;  // Angle
+  double d_angle;  // Angular velocity
+  double position;  // Position
+  double d_position; // Linear velocity
+};
+
 class Control : public rclcpp::Node {
  public:
   Control();
@@ -18,16 +27,24 @@ class Control : public rclcpp::Node {
   void Sleep();
   void Publish(float force_setpoint);
   [[nodiscard]] bool NodeOk() const { return !stop_node_; }
-
+ private:
+  double SwingUp(const State &state);
  private:
   bool stop_node_;
   std::chrono::time_point<std::chrono::steady_clock> t_start_;
   std::chrono::time_point<std::chrono::steady_clock> t_end_;
-  float kTimerSleep = 1.f/100.f;
+  float kTimerSleep = 1.f / 100.f;
 
-  std_msgs::msg::Float32 message_;
+  std_msgs::msg::Float32 float_message_;
   rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr publisher_;
 
+  rclcpp::Subscription<geometry_msgs::msg::Vector3>::SharedPtr pendulum_subscription_;
+  void PendulumCallback(const geometry_msgs::msg::Vector3 &msg);
+
+  rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr cart_subscription_;
+  void CartCallback(const std_msgs::msg::Float32 &msg);
+
+  ivp::State state_{};
 };
 
 // namespace ivp
--- ros2/src/ivp_control/src/ivp_control.cc
+++ ros2/src/ivp_control/src/ivp_control.cc
@@ -3,6 +3,8 @@
 namespace ivp {
 Control::Control() : Node("IvpControlNode") {
   stop_node_ = false;
+  state_ = State{};
+  std::cout << state_.angle << state_.d_angle << state_.position << state_.d_position << std::endl;
   std::cout << "Hello" << stop_node_ << std::endl;
 }
 
@@ -12,13 +14,32 @@ Control::~Control() {
 
 bool Control::Configure() {
   publisher_ = this->create_publisher<std_msgs::msg::Float32>("ivp/force_setpoint", rclcpp::SensorDataQoS());
+
+  pendulum_subscription_ = this->create_subscription<geometry_msgs::msg::Vector3>("ivp/pendulum_state",
+                                                                                  rclcpp::SensorDataQoS(),
+                                                                                  std::bind(&Control::PendulumCallback,
+                                                                                            this,
+                                                                                            std::placeholders::_1));
+  cart_subscription_ = this->create_subscription<std_msgs::msg::Float32>("ivp/cart_position",
+                                                                         rclcpp::SensorDataQoS(),
+                                                                         std::bind(&Control::CartCallback,
+                                                                                   this,
+                                                                                   std::placeholders::_1));
+
   return true;
 }
 
 void Control::RunOnce() {
   t_start_ = std::chrono::steady_clock::now();  //Begin execution timer
-  float test = 0.01;
-  Publish(test);
+//  auto torque = static_cast<float>(SwingUp(state_));
+//  Publish(torque);
+
+//  std::cout << "------------------------------------------------------------------" << std::endl;
+//  std::cout << "angle: " << state_.angle << std::endl;
+//  std::cout << "d_angle: " << state_.d_angle << std::endl;
+//  std::cout << "position: " << state_.position << std::endl;
+//  std::cout << "------------------------------------------------------------------" << std::endl;
+
 }
 
 void Control::Sleep() {
@@ -31,13 +52,44 @@ void Control::Sleep() {
     RCLCPP_WARN(this->get_logger(),
                 "Loop time: %f[ms], Overtime: %f[s] ",
                 t_duration.count() * 1000,
-                kTimerSleep - t_duration.count());
+                t_duration.count() - kTimerSleep);
   }
 }
 
 void Control::Publish(float force_setpoint) {
-  message_.data = force_setpoint;
-  publisher_->publish(message_);
+  float_message_.data = force_setpoint;
+  publisher_->publish(float_message_);
+}
+
+double Control::SwingUp(const State &state) {
+  double e_p = 0.0f;
+  double I_p = 0.00466;
+  double m_p = 0.071f;
+  double m_c = 0.288f;
+  double L_p = (0.685f - 0.246f);
+  double g = 9.81f;
+  double F_m = 0;
+  double b_c = 0.095f;
+  double e_t = m_p * g * L_p;
+  // 1/2 * masspole * (2 * length)**2 / 3 *  x[3]**2 + np.cos(x[2]) * polemass_length * gravity
+  // 0.5 * mv² + cos(theta)*m_p*l_p*g
+  // 0.5 * m*(r*w)² + cos(theta)*m_p*l_p*g
+  // 0.5 * m*r²*w² + cos(theta)*m_p*l_p*g
+  // E = 0.5 * J * w² + mgh, h = l*cos(theta)
+  //  if (state.angle > 1.571 || state.angle < 4.712) {
+//    e_p = 0.5 * I_p * state.d_angle * state.d_angle + m_p * g * L_p * cos(state.angle);
+    e_p = m_p * g * L_p * cos(state.angle);
+    double value = ((e_t - e_p) * state.d_angle * cos(state.angle));
+    return value;
+}
+
+void Control::PendulumCallback(const geometry_msgs::msg::Vector3 &msg) {
+  state_.angle = msg.x;
+  state_.d_angle = msg.y;
+}
+
+void Control::CartCallback(const std_msgs::msg::Float32 &msg) {
+  state_.position = msg.data;
 }
 
 // namespace ivp
